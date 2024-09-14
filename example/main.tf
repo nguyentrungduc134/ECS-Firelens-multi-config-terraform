41e4e5736a3e77b49931d4ebf0b9f33d17b0f550
nguyentrungduc134 <86754554+nguyentrungduc134@users.noreply.github.com>
2024-09-14 13:07:29 +0700

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,5 @@
 module "ecs_firelnes" {
   source  = ".."
-<!-- x-release-please-start-version -->
-  ```
-    Version : '0.1.6'
-  ```
-<!-- x-release-please-end -->
+    Version : '0.1.6' # x-release-please-version
 }
 
